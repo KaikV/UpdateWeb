e9d0c6082a6eefdfdf67eafb804705f6c26de3ca
Kaik Victor <kaikev035@gmail.com>
2023-02-23 15:21:18 -0400

    First controller adn Razor pages tests

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWeb.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["email"] = "kaikev045@gmail.com";
             return View();
         }
 
